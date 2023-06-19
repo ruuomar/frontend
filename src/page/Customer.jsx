e3ae1b7213044a8d9fc826e61066bae7a5c4718e
ruuomar <orukayya605@gmail.com>
2023-06-19 12:41:59 -0700

    first commit

---
+++ src/page/Customer.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+import './style.css'
+function Customer() {
+    // kwajili ya mabadiliko hii ni react hock
+//    const [customer,set,setcustomer]=useState([]);
+//    const fetch=()=>
+//    customerService.getcust().then((Response))=>
+  return (
+    <div>
+    <table>
+    <tr>
+    <th>KISWAHILI</th>
+    <th>KITENDO</th>
+    <th>WAKATI WA SASA</th>
+</tr>
+<tr>
+    <td></td>
+    <td></td>
+    <td></td>
+</tr>
+
+</table>
+    
+    </div>
+  )
+}
+
+export default  Customer
+
+// useDefate inatumika katika PaymentRequestUpdateEvent,create ambayo tunapitisha kufech data
